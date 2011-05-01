e1fd28ecb81218840d230a01a1e3e6baf264b254
Tom Krush <t.krush@gmail.com>
2011-05-01 14:35:14 -0500

    Added example code to work with relationship functionality.

--- controllers/pages.php
+++ controllers/pages.php
@@ -14,18 +14,21 @@ class Pages extends CI_Controller
 		$this->load->database();
 
 		
-		$blog = $this->blogs_model->create(array(
-			'name' => 'test',
-			'slug' => 'test'
-		));
+		// $blog = $this->blogs_model->create(array(
+		// 	'name' => 'test',
+		// 	'slug' => 'test'
+		// ));
 		
-		// Array of errors if validated
-		$errors = $this->blogs_model->errors;
-		print_r($errors);
-
-		if ( $blog )
-		{
-			print_r($blog->row());
-		}		
+		$blog = $this->blogs_model->first(8)->row();
+		var_dump($blog->articles->first()->row()->contents);
+		
+		// // Array of errors if validated
+		// $errors = $this->blogs_model->errors;
+		// print_r($errors);
+		// 
+		// if ( $blog )
+		// {
+		// 	print_r($blog->row());
+		// }		
 	}
 }
--- models/blogs_model.php
+++ models/blogs_model.php
@@ -7,5 +7,7 @@ class Blogs_Model extends My_Model
 		$this->fields('rss_url', 'slug', 'name', 'description');
 		$this->validates('name', array('presence' => TRUE, 'uniqueness' => TRUE));
 		$this->validates('slug', array('presence' => TRUE, 'uniqueness' => TRUE));
+		
+		$this->has_many('articles');
 	}
 }
