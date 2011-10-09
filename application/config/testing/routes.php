0df199c05bf22211d1333064e108ab2b6538712d
Tom Krush <t.krush@gmail.com>
2011-10-09 15:27:15 -0500

    Cleaned up routes.

--- application/config/testing/routes.php
+++ application/config/testing/routes.php
@@ -5,10 +5,10 @@ $route['migrations/seed']				= "migrations/seed";
 $route['migrations/reset']				= "migrations/reset";
 $route['migrations'] 					= 'migrations/index';
 
-$route['unit/javascript/(:any)'] = 'unit/javascript/$1';
-$route['unit/images/(:any)'] = 'unit/images/$1';
-$route['unit/stylesheets/(:any)'] = 'unit/stylesheets/$1';
-$route['(:any)'] = 'unit';
+$route['unit/javascript/(:any)'] 		= 'unit/javascript/$1';
+$route['unit/images/(:any)'] 			= 'unit/images/$1';
+$route['unit/stylesheets/(:any)'] 		= 'unit/stylesheets/$1';
+$route['(:any)'] 						= 'unit';
 
-$route['default_controller'] = "unit";
-$route['404_override'] = '';
+$route['default_controller'] 			= "unit";
+$route['404_override'] 					= '';
